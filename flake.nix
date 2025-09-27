c0c25f6fcc6b295e2ccda570875a4001dc584e0b
Bearofbusiness <61521224+bearofbusiness@users.noreply.github.com>
2025-09-27 05:30:50 -0400

    messed with flake and fixed hashes that wernt updated when updated ver see https://github.com/kassane/zig-esp-idf-sample/commit/f59113138ef141a3e6e60fd876994237f9b2ffb2

--- flake.nix
+++ flake.nix
@@ -2,63 +2,56 @@
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-    zls-overlay.url = "github:zigtools/zls/edd8f2264ea45f7370bac4aba809bc179d96627e";
+    # zls-overlay.url = "github:zigtools/zls/edd8f2264ea45f7370bac4aba809bc179d96627e";
     esp-idf.url = "github:mirrexagon/nixpkgs-esp-dev";
   };
 
-  outputs = { self, nixpkgs, flake-utils, zls-overlay, esp-idf, ... }:
+  outputs = { self, nixpkgs, flake-utils, esp-idf, ... }: # zls-overlay, 
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs {
           inherit system;
-          overlays = [
-            esp-idf.overlays.default
-            (final: prev: {
-              zls = zls-overlay.packages.${system}.default;
-            })
-          ];
+          overlays = [ esp-idf.overlays.default ];
         };
 
         # Determine architecture-specific properties
         platformSrc = if system == "x86_64-linux" then
           {
             url = "https://github.com/kassane/zig-espressif-bootstrap/releases/download/0.14.0-xtensa/zig-relsafe-x86_64-linux-musl-baseline.tar.xz";
-            sha256 = "sha256-KAFGYgUP1poW79QrLwq7JI07VyZayWbBDqcGT6NQbug=";
+            sha256 = "sha256-czEQX03pDNzoh9SGhWfs5miU7vK1md7sYCd3lHSLLCA=";
           }
         else if system == "aarch64-linux" then
           {
             url = "https://github.com/kassane/zig-espressif-bootstrap/releases/download/0.14.0-xtensa/zig-relsafe-aarch64-linux-musl-baseline.tar.xz";
-            sha256 = "sha256-1bnaav5a8gp108izghlcpdnl1y0rl5jzzmzx47q234k3h5m42jb2";
+            sha256 = "sha256-MUrTa7hI1Gx4vCM+0Tnu5D7agoUWzPb11pufIBptFCQ=";
           }
         else
           throw "Unsupported platform: ${system}";
 
+        zigEsp = pkgs.stdenv.mkDerivation {
+          pname = "zig-espressif-bootstrap";
+          version = "0.14.0-xtensa";
+          src = pkgs.fetchurl platformSrc;
+          dontConfigure = true; dontBuild = true; dontFixup = true;
+          installPhase = ''
+            mkdir -p $out/{doc,bin,lib}
+            cp -r doc/* $out/doc
+            cp -r lib/* $out/lib
+            cp zig $out/bin/zig
+          '';
+        };
+
       in
-      with pkgs;
       {
-        formatter = nixpkgs.legacyPackages.x86_64-linux.nixpkgs-fmt;
-        devShells.default = mkShell {
+        formatter = pkgs.nixpkgs-fmt;
+        devShells.default = pkgs.mkShell {
           buildInputs = [
-            bashInteractive
+            pkgs.bashInteractive
 
-            (pkgs.stdenv.mkDerivation {
-              pname = "zig-espressif-bootstrap";
-              version = "0.14.0-xtensa-dev";
-              src = pkgs.fetchurl platformSrc;
-              dontConfigure = true;
-              dontBuild = true;
-              dontFixup = true;
-              installPhase = ''
-                mkdir -p $out/{doc,bin,lib}
-                cp -r doc/* $out/doc
-                cp -r lib/* $out/lib
-                cp zig $out/bin/zig
-              '';
-            })
+            zigEsp
+            pkgs.zls
 
-            zls
-
-            esp-idf-full
+            pkgs.esp-idf-full
           ];
         };
       }
